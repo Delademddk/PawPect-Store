83b76fcb53275d2b5c8eb96afce667f7e488359d
Delademddk <kegudeladem@gmail.com>
2026-03-28 22:13:32 +0000

    Created the navbar and added a user avatar

--- src/app/pages/PetDashboardPage.tsx
+++ src/app/pages/PetDashboardPage.tsx
@@ -1,20 +1,13 @@
 import NavBar from "@/components/NavBar";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
 
 export default function PetDashboardPage() {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.clear();
-    navigate("/login");
-  };
+  
   return (
     <div>
       <NavBar />
       <p>I am the dashboard Page</p>
-      <button className="p-2 bg-amber-400" onClick={handleLogout}>
-        Log Out
-      </button>
     </div>
   );
 }
